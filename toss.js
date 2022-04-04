340f15d03648fd8cd295be83ea1e1c26d47010b3
Katzengott <78551650+Katzengott@users.noreply.github.com>
2022-04-04 16:54:25 +0200

    Create toss.js

---
+++ toss.js
@@ -0,0 +1,7 @@
+async function toss(){
+        point = new Vec3(176, 67,-174)
+        await bot.lookAt(point)
+        //bot.blockAt(point)
+        await bot.toss(itemType, metadata, counts)
+        pay = true
+    }
